ddff4f39da082876798cae6da8a77b5f5cbae015
Mirko Cukich <mirkocukich@gmail.com>
2018-02-08 20:36:55 -0600

    Program Complete

---
+++ app/src/main/java/com/digitalmirko/materialcalendarviewdemo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.digitalmirko.materialcalendarviewdemo;
+
+import android.icu.util.Calendar;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MaterialCalendarView materialCalendarView = (MaterialCalendarView) findViewById(R.id.calendarView);
+
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(1910, 1,1))
+                .setMaximumDate(CalendarDay.from(2150,12,31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Toast.makeText(MainActivity.this, "Selected: " + date.getDate() , Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
